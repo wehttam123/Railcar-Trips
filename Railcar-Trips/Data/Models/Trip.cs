d0b6937dafd52953dc42e39023a35948e9550203
wehttam123 <wehttamh11@gmail.com>
2026-03-04 21:11:33 -0700

    Update Trip model

--- Railcar-Trips/Data/Models/Trip.cs
+++ Railcar-Trips/Data/Models/Trip.cs
@@ -3,10 +3,10 @@ namespace Railcar_Trips.Data.Models;
 public class Trip
 {
     public int Id { get; set; }
-    public int EquipmentId { get; set; }
+    public string EquipmentId { get; set; }
     public int? OriginCityId { get; set; }
     public int? DestinationCityId { get; set; }
-    public string? StartUTC { get; set; }
-    public string? EndUTC { get; set; }
+    public DateTime? StartUTC { get; set; }
+    public DateTime? EndUTC { get; set; }
     public int? TotalTripHours { get; set; } = 0;
 }
---
+++ Railcar-Trips/Migrations/20260305041110_UpdateTrips.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Railcar_Trips.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrips : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartUTC",
+                table: "Trips",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EquipmentId",
+                table: "Trips",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndUTC",
+                table: "Trips",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StartUTC",
+                table: "Trips",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EquipmentId",
+                table: "Trips",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndUTC",
+                table: "Trips",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
